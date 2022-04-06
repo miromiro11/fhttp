494639da626c7e65fe93a4d621da383a423cdb91
Daniel <saucesteals@gmail.com>
2022-04-06 06:45:54 -0700

    wrapper for fhttp -> http

--- wrappers_test.go
+++ wrappers_test.go
@@ -28,3 +28,24 @@ func TestStdlibTransport(t *testing.T) {
 	}
 
 }
+
+func TestFHttpTransport(t *testing.T) {
+	var txp fhttp.RoundTripper = &fhttp.FHttpTransport{
+		Transport: &http.Transport{},
+	}
+	ctx, cancel := context.WithCancel(context.Background())
+	cancel() // we expect the request with context to fail immediately
+	req, err := fhttp.NewRequestWithContext(
+		ctx, "GET", "https://google.com", nil)
+	if err != nil {
+		t.Fatal(err) // should not fail
+	}
+	resp, err := txp.RoundTrip(req)
+	if !errors.Is(err, context.Canceled) {
+		t.Fatal("unexpected err", err)
+	}
+	if resp != nil {
+		t.Fatal("unexpected resp")
+	}
+
+}
